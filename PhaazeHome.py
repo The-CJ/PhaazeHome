ee23d90a3b5e4c42bca6eb43bb2f64ec9710a590
The-CJ <christoperjaekel@googlemail.com>
2018-08-24 00:44:03 +0200

    to many misstakes

--- PhaazeHome.py
+++ PhaazeHome.py
@@ -13,12 +13,12 @@ class PhaazeHome(object):
 		self.out_pins = [3, 5, 7, 11, 13, 15, 19, 21]
 		self.setup_pins()
 
-	from pins import pins, setup_pins
+	from pin_controll import pins, setup_pins
 
 PH = PhaazeHome()
 
 SERVER = web.Application()
-SERVER.add_route('GET', '/pins{useless:.*}', PH.pins)
+SERVER.router.add_route('GET', '/pins{useless:.*}', PH.pins)
 
 web.run_app(SERVER, port=80)
-GPIO.clearup()
+GPIO.cleanup()
