7291a1e5ab0d4cfaccc05c218e9df9602038fe02
The-CJ <christoperjaekel@googlemail.com>
2018-08-24 18:46:06 +0200

    tab softing

--- interface.py
+++ interface.py
@@ -1,17 +1,17 @@
 import asyncio, mimetypes
 
 async def web_interface(self, request):
-    path = request.path
+	path = request.path
 
 	if path == "/web":
 		path = "/web/html/master.html"
 
 	path = path.replace("..", "")
-    path = path.strip('/')
+	path = path.strip('/')
 
-    try:
-        r = open(path, "rb").read()
-        return self.response(status=200, body=r, headers={'Content-Type': mimetypes.guess_type(path)[0]})
+	try:
+		r = open(path, "rb").read()
+		return self.response(status=200, body=r, headers={'Content-Type': mimetypes.guess_type(path)[0]})
 
-    except:
-        return self.response(status=400)
+	except:
+		return self.response(status=400)
