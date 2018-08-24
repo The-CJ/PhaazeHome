fcc33aabc8a9b8658ddca6c008018b7d104ee85d
The-CJ <christoperjaekel@googlemail.com>
2018-08-24 18:14:29 +0200

    added all main files

--- PhaazeHome.py
+++ PhaazeHome.py
@@ -12,13 +12,27 @@ class PhaazeHome(object):
 		self.version = "0.5"
 		self.out_pins = [3, 5, 7, 11, 13, 15, 19, 21]
 		self.setup_pins()
+		self.response = self.send_back_response
 
 	from pin_controll import pins, setup_pins
+	from interface import web_interface
+
+	def send_back_response(self, **kwargs):
+		already_set_header = kwargs.get('headers', dict())
+		kwargs['headers'] = already_set_header
+		kwargs['headers']['server'] =f"PhaazeHome v{self.version}"
+
+		if kwargs['headers'].get('Content-Type', None) == None:
+			kwargs['headers']['Content-Type'] ="Application/json"
+
+		return web.Response(**kwargs)
+
 
 PH = PhaazeHome()
 
 SERVER = web.Application()
-SERVER.router.add_route('GET', '/pins{useless:.*}', PH.pins)
+SERVER.router.add_route('*', '/pins{useless:.*}', PH.pins)
+SERVER.router.add_route('GET', '/web{path:.*}', PH.web_interface)
 
 web.run_app(SERVER, port=80)
 GPIO.cleanup()
---
+++ interface.py
@@ -0,0 +1,14 @@
+import asyncio, mimetypes
+
+async def web_interface(self, request):
+    path = request.path
+
+	path = path.replace("..", "")
+    path = path.strip('/')
+
+    try:
+        r = open(path, "rb").read()
+        return self.response(status=200, body=r, headers={'Content-Type': mimetypes.guess_type(path)[0]})
+
+    except:
+        return self.response(status=400)
